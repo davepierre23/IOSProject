58f4a7e5360c3c9c4179c7ddc31ba60683021e87
Dave Pierre <soccermsndave@hotmail.com>
2021-03-20 19:19:22 -0400

    menu is working

--- FinanceApp/FinanceApp/ViewController.swift
+++ FinanceApp/FinanceApp/ViewController.swift
@@ -10,7 +10,7 @@ import SideMenu
 
 class ViewController: UIViewController {
 
-    private let sideMenu = SideMenuNavigationController(rootViewController: UIViewController());
+    private let sideMenu = SideMenuNavigationController(rootViewController: MenuController(with: ["Home","Info","Setting"]));
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -38,26 +38,44 @@ class ViewController: UIViewController {
     
 }
 class MenuController: UITableViewController{
-//    private let menuItems!: [String];
-//
-//    init(with menuItems: [String]){
-//        super.init(nibName:nil,bundle:nil)
-//    }
+    private let menuItems: [String];
+
+    init(with menuItems: [String]){
+        self.menuItems = menuItems
+        super.init(nibName:nil,bundle:nil)
+      
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     // Table methods
     
-//    override func tableView(_ tableView:UITableView,numberOfRowsInSection section :Int)->Int{
-//        return menuItems.count;
-//    }
-//    override func tableView(_ tableView:UITableView,cellForRowAt IndexPath: IndexPath :Int)->UITableViewCell{
-//        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for:IndexPath)
-//        cell.texxt
-//        return menuItems.count;
-//    }
-//
+    override func tableView(_ tableView:UITableView,numberOfRowsInSection section :Int)->Int{
+        return menuItems.count;
+    }
+    
+    override func viewDidLoad() {
+        tableView.backgroundColor = .darkGray
+        view.backgroundColor = .darkGray;
+    }
+    override func tableView(_ tableView:UITableView,cellForRowAt indexPath: IndexPath )-> UITableViewCell{
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for:indexPath)
+        cell.textLabel?.text = menuItems[indexPath.row]
+        cell.textLabel?.textColor = .white
+        cell.backgroundColor = .darkGray
+        cell.contentView.backgroundColor = .darkGray
+        
+        return cell;
+    }
+    //used to handle when a tablerow has beww selcted
+    override func tableView(_ tableView:UITableView,didSelectRowAt indexPath: IndexPath){
+        tableView.deselectRow(at: indexPath, animated: true)
+    
+        //tell app to go to the next view
+    
+    }
     
     
 }
