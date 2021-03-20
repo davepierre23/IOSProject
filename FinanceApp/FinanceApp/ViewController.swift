7d1b30ad27bd56047497cc87b05b9b30bc4aa128
Dave Pierre <soccermsndave@hotmail.com>
2021-03-20 18:35:01 -0400

    added coco pods and menu items

--- FinanceApp/FinanceApp/ViewController.swift
+++ FinanceApp/FinanceApp/ViewController.swift
@@ -6,14 +6,59 @@
 //
 
 import UIKit
+import SideMenu
 
 class ViewController: UIViewController {
 
+    private let sideMenu = SideMenuNavigationController(rootViewController: UIViewController());
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        sideMenu.leftSide = true
+        SideMenuManager.default.leftMenuNavigationController = sideMenu
+        SideMenuManager.default.addPanGestureToPresent(toView: view)
+
+        
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 250, height: 50));
+        
+        view.addSubview(button);
+        
+        button.backgroundColor = .systemPurple;
+        button.center = view.center;
+        button.setTitle("Show Tab Bar", for: .normal);
+        button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside);
+        
     }
 
-
+    @IBAction func didTapButton(){
+        present(sideMenu, animated: true)
+    }
+    
+    
+}
+class MenuController: UITableViewController{
+//    private let menuItems!: [String];
+//
+//    init(with menuItems: [String]){
+//        super.init(nibName:nil,bundle:nil)
+//    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    // Table methods
+    
+//    override func tableView(_ tableView:UITableView,numberOfRowsInSection section :Int)->Int{
+//        return menuItems.count;
+//    }
+//    override func tableView(_ tableView:UITableView,cellForRowAt IndexPath: IndexPath :Int)->UITableViewCell{
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for:IndexPath)
+//        cell.texxt
+//        return menuItems.count;
+//    }
+//
+    
+    
 }
 
---
+++ FinancePlannerApp/FinancePlannerApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FinancePlannerApp
+//
+//  Created by Dave Pierre on 2021-03-20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
