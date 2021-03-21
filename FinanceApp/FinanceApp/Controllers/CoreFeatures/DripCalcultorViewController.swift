919efab680820cdfb0117226fe7897974ba104a8
Dave Pierre <soccermsndave@hotmail.com>
2021-03-21 09:46:13 -0400

    change the menu to be tabbar

--- FinanceApp/FinanceApp/Controllers/CoreFeatures/DripCalcultorViewController.swift
+++ FinanceApp/FinanceApp/Controllers/CoreFeatures/DripCalcultorViewController.swift
@@ -9,21 +9,50 @@ import UIKit
 
 class DripCalcultorViewController: UIViewController {
 
+
+    private let searchBar: UISearchBar = {
+        let searchBar = UISearchBar();
+        searchBar.backgroundColor = .red
+        return searchBar
+    }()
+    
+    private var collectionView: UICollectionView?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .secondarySystemBackground
+        navigationController?.navigationBar.topItem?.titleView = searchBar
 
-        // Do any additional setup after loading the view.
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical;
+        layout.sectionInset = UIEdgeInsets(top: 0
+                                        , left: 0,
+                                           bottom: 0,
+                                           right: 0)
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView?.delegate = self
+        collectionView?.dataSource = self
+        guard let collection = collectionView else {
+            return
+        }
+        
+        view.addSubview(collectionView!)
+        
     }
+}
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+extension  DripCalcultorViewController:  UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
     }
-    */
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell();
+    }
+    
+    
 
 }
